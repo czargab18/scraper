502813475535a8f4256f95b45abbf6960e53d1dc
--global <--global>
2025-07-04 21:44:37 -0300

    adiciona arquivo HTML para exibição de docentes e atualiza spider para coletar dados de departamentos

--- sigaa/sigaa/spiders/docentes.py
+++ sigaa/sigaa/spiders/docentes.py
@@ -1,13 +1,21 @@
 import scrapy
 
-
 class DocentesSpider(scrapy.Spider):
     name = "docentes"
     allowed_domains = ["sigaa.unb.br"]
     start_urls = ["https://sigaa.unb.br/sigaa/public/docente/busca_docentes.jsf?aba=p-academico"]
 
     def parse(self, response):
-        for departamento in response.css("select#form:departamento"):
-            yield response.follow(departamento, self.parse_departamento)
+        departamentos = response.css("select#form\\:departamento option")
+        for opcao in departamentos:
+            valor = opcao.attrib.get("value")
+            texto = opcao.css("::text").get()
+            if valor and valor not in ["", "0"]:
+                yield {
+                    # código = nome departamento
+                    valor : texto.strip()
+                }
 
 
+#> cd sigaa
+# > uv run scrapy runspider .\sigaa\spiders\docentes.py -o data\docentes\departamentos.json
