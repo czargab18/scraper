851ad1cf6f061c57b1b087c0bb79f71856dbbf91
czargab18 <87340795+czargab18@users.noreply.github.com>
2025-07-24 12:03:01 -0300

    feat: atualizar URL de início do spider ofertas para nova estrutura

--- sigaa/sigaa/spiders/ofertas.py
+++ sigaa/sigaa/spiders/ofertas.py
@@ -4,5 +4,5 @@ import os
 class OfertasSpider(scrapy.Spider):
     name = "ofertas"
     allowed_domains = ["sigaa.unb.br"]
-    start_urls = ["https://sigaa.unb.br/sigaa/public/curso/lista.jsf?nivel=G"]
+    start_urls = ["https://sigaa.unb.br/sigaa/public/curso/lista.jsf"]
     
